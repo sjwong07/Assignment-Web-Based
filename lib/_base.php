1a99e5a0135145ee8f456f81bb48f12a7daa6e5f
JoeyEngg <joeyeng1225@gmail.com>
2026-03-27 17:18:50 +0800

    cart & base(incomplete)

---
+++ lib/_base.php
@@ -0,0 +1,149 @@
+<?php
+
+function is_get() {
+    return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
+
+function is_post() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function get($key, $value = null) {
+    $value = $_GET[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
+
+function post($key, $value = null) {
+    $value = $_POST[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
+
+function req($key, $value = null) {
+    $value = $_REQUEST[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
+
+function redirect($url = null) {
+    $url ??= $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit();
+}
+
+function temp($key, $value = null) {
+    if ($value !== null) {
+        $_SESSION["temp_$key"] = $value;
+    }
+    else {
+        $value = $_SESSION["temp_$key"] ?? null;
+        unset($_SESSION["temp_$key"]);
+        return $value;
+    }
+}
+
+function encode($value) {
+    return htmlentities($value);
+}
+
+function html_hidden($key, $attr = '') {
+    $value ??= encode($GLOBALS[$key] ?? '');
+    echo "<input type='hidden' id='$key' name='$key' value='$value' $attr>";
+}
+
+function html_select($key, $items, $default = '- Select One -', $attr = '') {
+    $value = encode($GLOBALS[$key] ?? '');
+    echo "<select id='$key' name='$key' $attr>";
+    if ($default !== null) {
+        echo "<option value=''>$default</option>";
+    }
+    foreach ($items as $id => $text) {
+        $state = $id == $value ? 'selected' : '';
+        echo "<option value='$id' $state>$text</option>";
+    }
+    echo '</select>';
+}
+
+// ============================================================================
+// Error Handlings
+// ============================================================================
+
+// Global error array
+$_err = [];
+
+function err($key) {
+    global $_err;
+    if ($_err[$key] ?? false) {
+        echo "<span class='err'>$_err[$key]</span>";
+    }
+    else {
+        echo '<span></span>';
+    }
+}
+
+// ============================================================================
+// Shopping Cart
+// ============================================================================
+
+// Get shopping cart
+function get_cart() {
+    return $_SESSION['cart'] ?? [];
+}
+
+// Set shopping cart
+function set_cart($cart = []) {
+    $_SESSION['cart'] = $cart;
+}
+
+// Update shopping cart
+function update_cart($id, $unit) {
+    $cart = get_cart();
+
+    if($unit >= 1 && $unit <= 10 && is_exists($id, 'product', 'id')){
+        $cart[$id] = $unit;
+        ksort($cart);
+    }
+    else{
+        unset($cart[$id]);
+    }
+
+    set_cart($cart);
+}
+
+// ============================================================================
+// Database Setups and Functions
+// ============================================================================
+
+// Global PDO object
+$_db = new PDO('mysql:dbname=dba', 'root', '', [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+]);
+
+// Is unique?
+function is_unique($value, $table, $field) {
+    global $_db;
+    $stm = $_db->prepare("SELECT COUNT(*) FROM $table WHERE $field = ?");
+    $stm->execute([$value]);
+    return $stm->fetchColumn() == 0;
+}
+
+// Is exists?
+function is_exists($value, $table, $field) {
+    global $_db;
+    $stm = $_db->prepare("SELECT COUNT(*) FROM $table WHERE $field = ?");
+    $stm->execute([$value]);
+    return $stm->fetchColumn() > 0;
+}
+
+// ============================================================================
+// Global Constants and Variables
+// ============================================================================
+
+$_units = array_combine(range(1,10), range(1,10));
+
+$_categories = [
+    'SAMSUNG' => 'SAMSUNG',
+    'OPPO'    => 'OPPO',
+    'IPHONE'  => 'IPHONE',
+    'IPAD'    => 'IPAD',
+    'VIVO'    => 'VIVO',
+    'XIAOMI'  => 'XIAOMI'
+];
---
+++ order/cart.php
@@ -0,0 +1,98 @@
+<?php
+include '../_base.php';
+
+// ----------------------------------------------------------------------------
+
+if (is_post()) {
+    $btn = req('btn');
+    if ($btn == 'clear'){
+        set_cart();
+        redirect('?');
+    }
+
+    $id   = req('id');
+    $unit = req('unit');
+    update_cart($id, $unit);
+    redirect();
+}
+
+// ----------------------------------------------------------------------------
+
+$_title = 'Order | Shopping Cart';
+include '../_head.php';
+?>
+
+<style>
+    .popup {
+        width: 100px;
+        height: 100px;
+    }
+</style>
+
+<table class="table">
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Price (RM)</th>
+        <th>Unit</th>
+        <th>Subtotal (RM)</th>
+    </tr>
+
+    <?php
+        $count =  0;
+        $total = 0;
+        $item = [];
+
+        $stm = $_db->prepare('SELECT * FROM product WHERE id = ?');
+        $cart = get_cart();
+
+        foreach($cart as $id => $unit):
+            $stm->execute([$id]);
+            $p = $stm->fetch();
+
+            $subtotal = $p->price * $unit;
+            $count += $unit;
+            $total += $subtotal;
+    ?>
+        <tr>
+            <td><?= $p->id ?></td>
+            <td><?= $p->name ?></td>
+            <td class="right"><?= $p->price ?></td>
+            <td>
+                <form method="post">
+                    <?=  html_hidden('id') ?>
+                    <?=  html_select('unit', $_units, '') ?>
+                </form>            
+            </td>
+            <td class="right">
+                <?= sprintf('%.2f', $subtotal) ?>
+                <img src="/products/<?= $p->photo ?>" class="popup">
+            </td>
+        </tr>
+    <?php endforeach ?>
+
+    <tr>
+        <th colspan="3"></th>
+        <th class="right"><?= $count ?></th>
+        <th class="right"><?= sprintf('%.2f', $total) ?></th>
+    </tr>
+</table>
+
+<p>
+    <?php if ($cart): ?>
+        <button data-post="?btn=clear">Clear</button>
+
+        <?php if ($_user?->role == 'Member'): ?>
+            <button data-post="checkout.php">Checkout</button>
+        <?php else: ?>
+            Please <a href="/login.php">login</a> as member to checkout
+        <?php endif ?>
+    <?php endif ?>
+</p>
+
+<script>
+    $('select').on('change', e => e.target.form.submit());
+</script>
+
+<?php
+include '../_foot.php';
