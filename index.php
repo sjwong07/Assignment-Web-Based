9a3ce7ba3ae57ba2a7cc9c439d327d6969dc19d8
sjwong07 <shengjiewong97@gmail.com>
2026-04-05 20:46:01 +0800

    fixing shit webpage 404 problem

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include  __DIR__. '/lib/_head.php';
+?>
+<?php
+require '_base.php';
+?>
+
+<div class= "container mt-5">
+<h1>Hello</h1>
+<p>Please choose your choice</p>
+</div>
+
+<?php 
+    include '_foot.php';
+?>
--- lib/_head.php
+++ lib/_head.php
@@ -3,22 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $_title ?? 'Web_Based' ?> </title>
-
-
+    <title><?= $_title ?? 'Untitled' ?></title>
+    <link rel="shortcut icon" href="/images/favicon.png">
+    <link rel="stylesheet" href="Css/app.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src = "/js/app.js"></script>
+    <script src="js/app.js"></script>
 </head>
 <body>
     <header>
-        <h1><a href="/">Web-Based Assignment</a></h1>
+        <h1><a href="/">Reusable Layout</a></h1>
+
     </header>
 <nav>
-    <a href="/">PUt here</a>
-    <a href="Product.php">Product Filtering</a>
+    <a href="/lib/index.php">Index</a>
+    <a href="/order/checkout.php">CheckOut</a>
+    <a href="/order/cart.php">Cart</a>
+    <a href="/Product.php">Product</a>
 </nav>
 <main>
-    <h1><?= $_title ?? 'Edit_here' ?> </h1>
-</main>
-</body>
-</html>
+    <h1><?= $_title ?? 'Untitle' ?></h1>
+
+    
